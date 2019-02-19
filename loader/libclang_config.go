9fdd5ed8ff2344f7e99ba53ac646dfaeac11ac7b
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-02-19 18:07:52 +0100

    wip: all: add Windows support

--- loader/libclang_config.go
+++ loader/libclang_config.go
@@ -4,8 +4,10 @@ package loader
 
 /*
 #cgo linux  CFLAGS: -I/usr/lib/llvm-7/include
-#cgo darwin CFLAGS: -I/usr/local/opt/llvm/include
 #cgo linux  LDFLAGS: -L/usr/lib/llvm-7/lib -lclang
+#cgo darwin CFLAGS: -I/usr/local/opt/llvm/include
 #cgo darwin LDFLAGS: -L/usr/local/opt/llvm/lib -lclang -lffi
+#cgo windows CFLAGS: -I"C:/Program Files/LLVM/include"
+#cgo windows LDFLAGS: -L"C:/Program Files/LLVM/lib" -lclang
 */
 import "C"
